ea447d51dfa905d671c7ffe6f6a10815feea1458
ParsifalRU <bunin.vania2015@yandex.ru>
2022-07-14 23:44:35 +0500

    code_clean_up_and_remove_actionbar

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,116 +1,93 @@
 package com.sirius.test_app
 
-
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import rvadapter.CommentAdapter
-import java.time.temporal.TemporalAdjusters.previous
-
 
 class MainActivity : AppCompatActivity() {
 
-    val dm = DataModel()
-    val adapter = CommentAdapter()
+    private val dm = DataModel()
+    private val adapter = CommentAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        recview()
-
+        initRecycler()
         loadImage()
-
-        loadlogo()
-
-        bars()
-
-        settv()
-
-        setrtb()
-
-        setbtn()
+        loadLogo()
+        initTextViews()
+        initRatingBar()
+        initButtons()
     }
 
-    fun loadImage() {
-        val imgv_dota = findViewById<ImageView>(R.id.imgv)
+    private fun loadImage() {
+        val dotaImage = findViewById<ImageView>(R.id.imgv)
         Picasso.with(this)
             .load(dm.image)
             .placeholder(R.drawable.ic_baseline_data_usage_24)
             .error(R.drawable.ic_launcher_foreground)
-            .into(imgv_dota)
-
+            .into(dotaImage)
     }
 
-    fun loadlogo(){
-        val imgv_logo = findViewById<ImageView>(R.id.img_logo)
+    private fun loadLogo() {
+        val logoImage = findViewById<ImageView>(R.id.img_logo)
 
         Picasso.with(this)
             .load(dm.logo)
             .placeholder(R.drawable.ic_baseline_data_usage_24)
             .error(R.drawable.ic_launcher_foreground)
-            .into(imgv_logo)
+            .into(logoImage)
     }
 
-    fun bars(){
-        supportActionBar?.hide()
-        actionBar?.hide()
+    private fun initTextViews() {
+        val title = findViewById<TextView>(R.id.tv_title)
+        title.text = dm.name
+
+        val marksCount = findViewById<TextView>(R.id.tv70)
+        marksCount.text = dm.gradeCnt
+
+        val tag1 = findViewById<TextView>(R.id.tv_moba)
+        tag1.text = dm.tags[0]
+
+        val tag2 = findViewById<TextView>(R.id.tv_multip)
+        tag2.text = dm.tags[1]
+
+        val tag3 = findViewById<TextView>(R.id.tv_strat)
+        tag3.text = dm.tags[2]
+
+        val description = findViewById<TextView>(R.id.tv_description)
+        description.text = dm.description
+
+        val review = findViewById<TextView>(R.id.tv_review)
+        review.text = dm.rev_rat
+
+        val reviewsCount = findViewById<TextView>(R.id.tv_numb_rev)
+        reviewsCount.text = "${dm.gradeCnt} ${dm.rev_rat.removeRange(6..15)}s"
+        val rating = findViewById<TextView>(R.id.tv_big_numb)
+        rating.text = dm.rating.toString()
     }
 
-    fun settv(){
-        val tv_title = findViewById<TextView>(R.id.tv_title)
-        tv_title.text = dm.name
+    private fun initRatingBar() {
+        val ratingBar1 = findViewById<RatingBar>(R.id.rtb)
+        ratingBar1.rating = dm.rating
 
-        val tv70 = findViewById<TextView>(R.id.tv70)
-        tv70.text = dm.gradeCnt
-
-        val tv_moba = findViewById<TextView>(R.id.tv_moba)
-        tv_moba.text = dm.tags[0]
-
-        val tv_mult = findViewById<TextView>(R.id.tv_multip)
-        tv_mult.text = dm.tags[1]
-
-        val tv_strat = findViewById<TextView>(R.id.tv_strat)
-        tv_strat.text = dm.tags[2]
-
-        val tv_description = findViewById<TextView>(R.id.tv_description)
-        tv_description.text = dm.description
-
-        val tv_review = findViewById<TextView>(R.id.tv_review)
-        tv_review.text = dm.rev_rat
-
-        val tv_numb_rev = findViewById<TextView>(R.id.tv_numb_rev)
-        tv_numb_rev.text = dm.gradeCnt + " " + dm.rev_rat.removeRange(6..15) + "s"
-
-        val tv_big_numb = findViewById<TextView>(R.id.tv_big_numb)
-        tv_big_numb.text = dm.rating.toString()
-
-
+        val ratingBar2 = findViewById<RatingBar>(R.id.rtb_2)
+        ratingBar2.rating = dm.rating
     }
 
-    fun setrtb(){
-        val rtb = findViewById<RatingBar>(R.id.rtb)
-        rtb.rating = dm.rating
-
-        val rtb_2 = findViewById<RatingBar>(R.id.rtb_2)
-        rtb_2.rating = dm.rating
+    private fun initButtons() {
+        val installButton = findViewById<Button>(R.id.btn)
+        installButton.setBackgroundColor(resources.getColor(R.color.yel_more))
+        installButton.text = dm.action.name
     }
 
-    fun setbtn(){
-        val btn = findViewById<Button>(R.id.btn)
-        btn.setBackgroundColor(resources.getColor(R.color.yel_more))
-        btn.text = dm.action.name
-
+    private fun initRecycler() {
+        val recycler = findViewById<RecyclerView>(R.id.recyclerView)
+        recycler.layoutManager = LinearLayoutManager(this@MainActivity)
+        recycler.adapter = adapter
     }
-
-    fun recview(){
-        val rvw = findViewById<RecyclerView>(R.id.recyclerView)
-        rvw.layoutManager = LinearLayoutManager(this@MainActivity)
-        rvw.adapter = adapter
-    }
-
 }
--- app/src/main/java/rvadapter/CommentAdapter.kt
+++ app/src/main/java/rvadapter/CommentAdapter.kt
@@ -1,6 +1,5 @@
 package rvadapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,27 +9,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sirius.test_app.DataModel
 import com.sirius.test_app.MainActivity
 import com.sirius.test_app.R
+import com.sirius.test_app.ReviewModel
 import com.squareup.picasso.Picasso
 
-class CommentAdapter: RecyclerView.Adapter<CommentAdapter.CommentHolder>() {
+class CommentAdapter : RecyclerView.Adapter<CommentAdapter.CommentHolder>() {
 
-    val dm = DataModel()
-
-    class CommentHolder(view: View):RecyclerView.ViewHolder(view) {
-
-        val textvw0: TextView
-        val textvw1: TextView
-        val textvw2: TextView
-        val imgv: ImageView
-
-        init {
-            textvw0 = view.findViewById(R.id.tv_rec1)
-            textvw1 = view.findViewById(R.id.tv_rec2)
-            textvw2 = view.findViewById(R.id.tv_rec3)
-            imgv = view.findViewById(R.id.imgv)
-
-        }
-    }
+    private val dm = DataModel()
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): CommentHolder {
         val view = LayoutInflater.from(viewGroup.context)
@@ -40,20 +24,30 @@ class CommentAdapter: RecyclerView.Adapter<CommentAdapter.CommentHolder>() {
     }
 
     override fun onBindViewHolder(holder: CommentHolder, position: Int) {
-        Picasso.with(MainActivity().baseContext)
-            .load(dm.reviews[position].userImage)
-            .placeholder(R.drawable.ic_baseline_data_usage_24)
-            .error(R.drawable.ic_launcher_foreground)
-            .into(holder.imgv)
-
-        holder.textvw0.text = dm.reviews[position].userName
-        holder.textvw1.text = dm.reviews[position].date
-        holder.textvw2.text = dm.reviews[position].message
-
+        holder.onBind(dm.reviews[position])
     }
 
     override fun getItemCount(): Int {
         return dm.reviews.size
     }
 
+    class CommentHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val username: TextView = view.findViewById(R.id.tv_rec1)
+        private val reviewDate: TextView = view.findViewById(R.id.tv_rec2)
+        private val review: TextView = view.findViewById(R.id.tv_rec3)
+        private val avatar: ImageView = view.findViewById(R.id.imgv)
+
+        fun onBind(reviewModel: ReviewModel) {
+            Picasso.with(MainActivity().baseContext)
+                .load(reviewModel.userImage)
+                .placeholder(R.drawable.ic_baseline_data_usage_24)
+                .error(R.drawable.ic_launcher_foreground)
+                .into(avatar)
+
+            username.text = reviewModel.userName
+            reviewDate.text = reviewModel.date
+            review.text = reviewModel.message
+        }
+    }
 }
